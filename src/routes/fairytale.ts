8fe716f2bec91e00a3d26a90caa73fad0ae221c7
Sayer(JangSeYoung) <55133871+Say-young@users.noreply.github.com>
2022-11-24 01:50:03 +0900

    [add] 생성된 문자열 split 처리 추가

--- src/routes/fairytale.ts
+++ src/routes/fairytale.ts
@@ -2,6 +2,6 @@ import express from 'express';
 const router = express.Router();
 const fairytaleController = require('../controllers/fairytaleController');
 
-router.post('/', fairytaleController.example);
+router.post('/', fairytaleController.trimNewSentence);
 
 module.exports = router;
