eb542f819e9160e44de63d0866e9c6e045a8aed4
Sayer(JangSeYoung) <55133871+Say-young@users.noreply.github.com>
2022-12-05 22:11:41 +0900

    [add] login 추가

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import userController from '../controllers/userController';
 const router = Router();
 
-//router.post('/', userController.login);
+router.post('/', userController.login);
 
 export default router;
