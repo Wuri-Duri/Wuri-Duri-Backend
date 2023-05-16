03be9d970058f9f5117b1114a49ac4753e5e53ee
Say-young <seyo_oung@ewhain.net>
2023-05-17 00:32:53 +0900

    [add] 동화 창작 관련 api 추가
    * ai와 user가 창작한 문장과 이미지 저장
    * 선택한 책 정보 불러오기

--- src/routes/fairytale.ts
+++ src/routes/fairytale.ts
@@ -6,7 +6,9 @@ const router = Router();
 
 //추후 checkUser middleware 추가 예정
 router.get('/main/:userIDX', fairytaleController.readAllBooks);
+router.get('/book/:ticketIDX', fairytaleController.readSelectedBook);
 router.post('/preset', fairytaleController.createNewTicket);
-router.post('/createcover', fairytaleController.addCoverInfo);
+router.post('/page', fairytaleController.addNewPage);
+router.post('/cover', fairytaleController.addCoverInfo);
 
 export default router;
